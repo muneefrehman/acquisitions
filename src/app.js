f08c1905826d75b7f030cc882ad1ddb64dbf6464
Muneef Rehman <muneefrehman@Muneefs-MacBook-Air.local>
2025-12-12 18:41:53 +0400

    Secure API with Arcjet

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import morgan from 'morgan';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import authRoutes from '#routes/auth.routes.js';
+import securityMiddleware from '#middleware/security.middleware.js';
 
 const app = express();
 
@@ -20,6 +21,8 @@ app.use(
 );
 
 const PORT = process.env.PORT || 3000;
+
+app.use(securityMiddleware);
 
 app.get('/', (req, res) => {
   logger.info('Hello from Acquisitions!');
