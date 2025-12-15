ab71ce5c65b3f393e60acdc46bfd82f077ef2c7c
Muneef Rehman <muneefrehman@Muneefs-MacBook-Air.local>
2025-12-15 10:38:28 +0400

    Implement CI/CD pipelines and GitHub actions

--- src/app.js
+++ src/app.js
@@ -48,4 +48,10 @@ app.get('/api', (req, res) => {
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 
+app.use((req, res) => {
+  res.status(404).json({
+    error: 'Route not found',
+  });
+});
+
 export default app;
